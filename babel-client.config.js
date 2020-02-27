1a663ff516812bbf32f89a89dacdae9033a6fb0c
mstream <maciej.laciak@gmail.com>
2020-02-27 17:33:10 +0000

    Improves browsers support

--- babel-client.config.js
+++ babel-client.config.js
@@ -2,7 +2,12 @@ module.exports = {
     babelrcRoots: ['.', 'client', 'common'],
     plugins: ['flow-runtime', '@babel/plugin-transform-block-scoping'],
     presets: [
-        '@babel/preset-env',
+        [
+            '@babel/preset-env',
+            {
+                targets: '> 0.25%, not dead'
+            },
+        ],
         '@babel/preset-flow',
         '@babel/preset-react'
     ],
